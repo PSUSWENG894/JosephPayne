634aabff03d53cadce69cf84ec6419eea9813eaa
Alex Payne <jap91@psu.edu>
2018-06-13 22:49:04 -0400

    Complete tutorial and add webpack.

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,11 +1,152 @@
 export class SimpleScene extends Phaser.Scene {
 
-    preload() {
-        this.load.image('bomb', 'assets/bomb.png');
+  constructor(test) {
+    super({
+      key: 'SimpleScene'
+    });
+  }
+
+  preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', {
+      frameWidth: 32,
+      frameHeight: 48
+    });
+  }
+
+  create() {
+
+    this.add.image(400, 300, 'sky');
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 0,
+        end: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{
+        key: 'dude',
+        frame: 4
+      }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', {
+        start: 5,
+        end: 8
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.stars = this.physics.add.group({
+      key: 'star',
+      repeat: 11,
+      setXY: {
+        x: 12,
+        y: 0,
+        stepX: 70
+      }
+    });
+
+    this.stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.bombs = this.physics.add.group();
+    this.scoreText = this.add.text(16, 16, 'score: 0', {
+      fontSize: '32px',
+      fill: '#000'
+    });
+    this.score = 0;
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.stars, this.platforms);
+    this.physics.add.collider(this.bombs, this.platforms);
+    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+  }
+
+  update() {
+    if (this.gameOver) {
+      return;
     }
 
-    create() {
-      this.add.text(100, 100, 'Hello Phaser! Look out of the bomb!', { fill: '#0f0' });
-      this.add.image(100, 200, 'bomb');
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play('left', true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play('right', true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play('turn');
+    }
+
+    if (this.cursors.up.isDown && (this.player.body.blocked.down || this.player.body.touching.down)) {
+      this.player.setVelocityY(-420);
     }
   }
+
+  collectStar(player, star) {
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    this.score += 10;
+    this.scoreText.setText(`Score ${this.score}`);
+    this.addBombs(this.stars.children.entries.filter(x => x.active !== true).length);
+    if (this.stars.countActive(true) < 5) {
+      this.resetStars();
+    }
+  }
+
+  hitBomb(player, bomb) {
+    this.physics.pause();
+    this.player.setTint(0xff0000);
+    this.player.anims.play('turn');
+    this.gameOver = true;
+  }
+
+  addBombs(starCount) {
+    let inactiveStarCountToDropBombs = 4;
+    if (isNaN(starCount) || starCount >= inactiveStarCountToDropBombs) {
+      let x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+      let bomb = this.bombs.create(x, 16, 'bomb');
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+      bomb.allowGravity = false;
+    }
+  }
+
+  resetStars() {
+    this.stars.children.iterate((child) => {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,79 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
+// Phaser webpack config
 var phaserModule = path.join(__dirname, '/node_modules/phaser/')
 var phaser = path.join(phaserModule, 'src/phaser.js')
 
+var definePlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+    WEBGL_RENDERER: true, // I did this to make webpack work, but I'm not really sure it should always be true
+    CANVAS_RENDERER: true // I did this to make webpack work, but I'm not really sure it should always be true
+})
+
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    vendor: ['phaser']
-  },
-
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: './build',
-    library: '[name]',
-    libraryTarget: 'umd',
-    filename: '[name].js'
-  },
-  
-  watch: true,
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src/'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
+    entry: {
+        app: [
+            'babel-polyfill',
+            path.resolve(__dirname, 'src/main.js')
+        ],
+        vendor: ['phaser']
+    },
+    devtool: 'cheap-source-map',
+    output: {
+      pathinfo: true,
+        path: path.resolve(__dirname, 'dev'),
+        publicPath: './dev/',
+        library: '[name]',
+        libraryTarget: 'umd',
+        filename: '[name].js'
+    },
+    watch: true, 
+    plugins: [
+        definePlugin,
+        //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */ }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: './src/index.html',
+            chunks: ['vendor', 'app'],
+            chunksSortMode: 'manual',
+            minify: {
+                removeAttributeQuotes: false,
+                collapseWhitespace: false,
+                html5: false,
+                minifyCSS: false,
+                minifyJS: false,
+                minifyURLs: false,
+                removeComments: false,
+                removeEmptyAttributes: false
+            },
+            hash: false
+        }),
+        new BrowserSyncPlugin({
+            host: process.env.IP || 'localhost',
+            port: process.env.PORT || 3000,
+            server: {
+                baseDir: ['./', './dev']
+            }
+        })
+    ],
+    module: {
+        rules: [
+            { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
+            { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
+            { test: [/\.vert$/, /\.frag$/], use: 'raw-loader' }
+        ]
+    },
+   /* node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    },
+    resolve: {
+        alias: {
+            'phaser': phaser,
         }
-      }
-    ]
-  },
-
-  devServer: {
-    contentBase: path.resolve(__dirname, 'build'),
-  },
-
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, 'index.html'),
-        to: path.resolve(__dirname, 'build')
-      },
-      {
-        from: path.resolve(__dirname, 'assets', '**', '*'),
-        to: path.resolve(__dirname, 'build')
-      }
-    ]),
-    
-    new webpack.DefinePlugin({
-        'typeof CANVAS_RENDERER': JSON.stringify(true),
-        'typeof WEBGL_RENDERER': JSON.stringify(true)
-    }),
-  ]
+    }*/
 }
