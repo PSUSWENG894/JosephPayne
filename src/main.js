361da7c2e5cae6f7cc51ba8a1a51db953b55d0b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-15 04:52:40 +0000

    Deploy PSUSWENG894/PhaserGame to github.com/PSUSWENG894/PhaserGame.git:gh-pages

---
+++ src/main.js
@@ -0,0 +1,22 @@
+
+import 'phaser';
+
+import {
+    SimpleScene
+} from './scenes/simple-scene';
+
+const gameConfig = {
+    type: Phaser.AUTO,
+    width: 680,
+    height: 400,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 500},
+            debug: false
+        }
+    },
+    scene: SimpleScene
+};
+
+new Phaser.Game(gameConfig);
