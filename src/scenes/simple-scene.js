a86ef7cd340efffc689685b11df54448ca04a33c
Alex Payne <jap91@psu.edu>
2018-07-07 00:22:10 -0400

    update simple scene

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,39 +1,35 @@
 export class SimpleScene extends Phaser.Scene {
-
   constructor(test) {
     super({
-      key: 'SimpleScene'
+      key: "SimpleScene"
     });
   }
 
   preload() {
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png", {
       frameWidth: 32,
       frameHeight: 48
     });
   }
 
   create() {
-    // this.add.image(400, 300, 'sky');
+    const maxHeight = this.cache.game.canvas.height;
+    const maxWidth = this.cache.game.canvas.width;
     this.platforms = this.physics.add.staticGroup();
-    this.platforms.maxSize = window.innerWidth;
-    this.platforms.create(900, 900, 'ground').setScale(2).refreshBody();
-    this.platforms.create(600, 400, 'ground');
-    this.platforms.create(90, 250, 'ground');
-    this.platforms.create(750, 220, 'ground');
-    this.platforms.create(500, 100, 'ground');
+    this.platforms.maxSize = maxWidth;
+    this.platforms.create(maxWidth * 0.5, maxHeight, "ground");
 
-    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player = this.physics.add.sprite(100, 450, "dude");
     this.player.setBounce(0.02);
     this.player.setCollideWorldBounds(true);
 
     this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', {
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", {
         start: 0,
         end: 3
       }),
@@ -42,17 +38,19 @@ export class SimpleScene extends Phaser.Scene {
     });
 
     this.anims.create({
-      key: 'turn',
-      frames: [{
-        key: 'dude',
-        frame: 4
-      }],
+      key: "turn",
+      frames: [
+        {
+          key: "dude",
+          frame: 4
+        }
+      ],
       frameRate: 20
     });
 
     this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', {
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", {
         start: 5,
         end: 8
       }),
@@ -62,7 +60,7 @@ export class SimpleScene extends Phaser.Scene {
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.stars = this.physics.add.group({
-      key: 'star',
+      key: "star",
       repeat: 11,
       setXY: {
         x: 12,
@@ -71,22 +69,34 @@ export class SimpleScene extends Phaser.Scene {
       }
     });
 
-    this.stars.children.iterate(function (child) {
+    this.stars.children.iterate(function(child) {
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     });
 
     this.bombs = this.physics.add.group();
-    this.scoreText = this.add.text(16, 16, 'score: 0', {
-      fontSize: '32px',
-      fill: '#000'
+    this.scoreText = this.add.text(16, 16, "score: 0", {
+      fontSize: "32px",
+      fill: "#000"
     });
     this.score = 0;
 
     this.physics.add.collider(this.player, this.platforms);
     this.physics.add.collider(this.stars, this.platforms);
     this.physics.add.collider(this.bombs, this.platforms);
-    this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
-    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      this.collectStar,
+      null,
+      this
+    );
+    this.physics.add.collider(
+      this.player,
+      this.bombs,
+      this.hitBomb,
+      null,
+      this
+    );
   }
 
   update() {
@@ -94,7 +104,7 @@ export class SimpleScene extends Phaser.Scene {
       this.resetBombs();
       this.resetStars();
       this.player.setRandomPosition();
-      this.player.anims.play('turn');
+      this.player.anims.play("turn");
       this.physics.resume();
       this.gameOver = false;
     }
@@ -102,18 +112,21 @@ export class SimpleScene extends Phaser.Scene {
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
 
-      this.player.anims.play('left', true);
+      this.player.anims.play("left", true);
     } else if (this.cursors.right.isDown) {
       this.player.setVelocityX(160);
 
-      this.player.anims.play('right', true);
+      this.player.anims.play("right", true);
     } else {
       this.player.setVelocityX(0);
 
-      this.player.anims.play('turn');
+      this.player.anims.play("turn");
     }
 
-    if (this.cursors.up.isDown && (this.player.body.blocked.down || this.player.body.touching.down)) {
+    if (
+      this.cursors.up.isDown &&
+      (this.player.body.blocked.down || this.player.body.touching.down)
+    ) {
       this.player.setVelocityY(-420);
     }
   }
@@ -124,7 +137,9 @@ export class SimpleScene extends Phaser.Scene {
     //  Add and update the score
     this.score += 10;
     this.scoreText.setText(`Score ${this.score}`);
-    this.addBombs(this.stars.children.entries.filter(x => x.active !== true).length);
+    this.addBombs(
+      this.stars.children.entries.filter(x => x.active !== true).length
+    );
     if (this.stars.countActive(true) < 5) {
       this.resetStars();
     }
@@ -133,16 +148,19 @@ export class SimpleScene extends Phaser.Scene {
   hitBomb(player, bomb) {
     this.physics.pause();
     this.player.setTint(0xff0000);
-    this.player.anims.play('turn');
+    this.player.anims.play("turn");
     this.gameOver = true;
   }
 
   addBombs(starCount) {
     let inactiveStarCountToDropBombs = 4;
     if (isNaN(starCount) || starCount >= inactiveStarCountToDropBombs) {
-      let x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+      let x =
+        this.player.x < 400
+          ? Phaser.Math.Between(400, 800)
+          : Phaser.Math.Between(0, 400);
 
-      let bomb = this.bombs.create(x, 16, 'bomb');
+      let bomb = this.bombs.create(x, 16, "bomb");
       bomb.setBounce(1);
       bomb.setCollideWorldBounds(true);
       bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
@@ -151,13 +169,13 @@ export class SimpleScene extends Phaser.Scene {
   }
 
   resetStars() {
-    this.stars.children.iterate((child) => {
+    this.stars.children.iterate(child => {
       child.enableBody(true, child.x, 0, true, true);
     });
   }
 
   resetBombs() {
-    this.bombs.children.iterate((child) => {
+    this.bombs.children.iterate(child => {
       child.disableBody(true, child.x, 0, true, true);
     });
   }
